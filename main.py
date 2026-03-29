a0ab1100b86592936f3bca42f4fde2bce9eacd8d
PATITAPABANKAR <142677190+PATITAPABANKAR@users.noreply.github.com>
2026-03-29 16:09:42 +0530

    Implement HTTP server to keep bot alive

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("GitHub setup working")
+import os
+
+print("Bot running on Render")
+
+# keep service alive
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT = int(os.environ.get("PORT", 10000))
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+server = HTTPServer(("0.0.0.0", PORT), handler)
+server.serve_forever()
